90460ee618568203b9165b81a4ff0e8ff23fed75
Yasamin Rahnavard <yas.rhd77@gmail.com>
2021-12-12 17:51:28 +0330

    Update assignment3_3.py

--- assignment3/assignment3_3.py
+++ assignment3/assignment3_3.py
@@ -10,3 +10,18 @@ if sorted_array == array:
     print("entered array is sorted")
 else:
     print("entered array is not sorted")
+    
+    
+    
+# without using sorted 
+array = []
+n = int(input("how many elements does the array have? "))
+for i in range(n):
+    num = int(input("enter element of the array: "))
+    array.append(num)
+print("entered array is: ", array)
+for i in range(1, len(a)):
+    if a[i] < a[i-1]:
+        print("entered array is not sorted")
+        exit()
+ print("entered array is sorted")
